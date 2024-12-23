0fe6799dece077e77ac77478c1bbedc65927138b
paul.andrei.ciocan <csokan.palandras@gmail.com>
2024-12-23 09:31:36 +0100

    Update: logo with festive decoration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  other: {
+    "google-adsense-account": "ca-pub-7900953467801418",
+  },
 };
 
 const ebg = localFont({
@@ -37,7 +40,7 @@ export default function RootLayout({
               <div className={styles.fejlec}>
                 <div className={styles.logo}>
                   <Image
-                    src="/logo-hu_1.18.3.png"
+                    src="/logo-hu_1.18.3_festive.png"
                     alt="Hungarian Wesnoth Logo"
                     width={415}
                     height={189}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,14 +9,7 @@ export default function Footer() {
     <>
       <div className={styles.footer}>
         <div>&copy; Magyar Wesnoth Közösség 2005-2021, 2024</div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <div className={styles.supporters}>
           Támogatók:{" "}
           <Image
             src="logo-react.svg"
@@ -25,6 +18,14 @@ export default function Footer() {
             alt="ReactJS Logo"
           />
           <Image src="logo-next.svg" width={18} height={18} alt="NextJS Logo" />
+        </div>
+        <div>
+          <a
+            href="https://www.freepik.com/free-vector/banner-ribbon-decoration-christmas-season_2920691.htm#fromView=search&page=1&position=14&uuid=5ed26483-71bc-4988-9845-6cf1e98fd517&new_detail=true"
+            target="_blank"
+          >
+            Festive Decoration by rawpixel.com on Freepik
+          </a>
         </div>
       </div>
     </>
