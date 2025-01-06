4c93fb8a08b94665bd571d1b595a94595a8bde68
paul.andrei.ciocan <csokan.palandras@gmail.com>
2025-01-06 12:31:58 +0100

    Remove: festive logo - end of holidays

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
               <div className={styles.fejlec}>
                 <div className={styles.logo}>
                   <Image
-                    src="/logo-hu_1.18.3_festive.png"
+                    src="/logo-hu_1.18.3.png"
                     alt="Hungarian Wesnoth Logo"
                     width={415}
                     height={189}
